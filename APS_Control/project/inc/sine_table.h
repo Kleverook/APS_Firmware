db23fd0b529430dec3a8063fa96ea2282315f85b
Kleverook <50455173+Kleverook@users.noreply.github.com>
2025-08-08 13:38:31 +0300

    Reconfig

---
+++ APS_Control/project/inc/sine_table.h
@@ -0,0 +1,8 @@
+//
+// Created by pader on 08.08.2025.
+//
+
+#ifndef SINE_TABLE_H
+#define SINE_TABLE_H
+
+#endif //SINE_TABLE_H
---
+++ APS_Control/project/src/sine_table.c
@@ -0,0 +1,5 @@
+//
+// Created by pader on 08.08.2025.
+//
+
+#include "sine_table.h"
