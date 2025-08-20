70a4a3c56e684345df01897053955d2481622eb2
Kleverook <50455173+Kleverook@users.noreply.github.com>
2025-08-20 20:05:18 +0300

    Add initial driver_tmr header and source files, and OpenOCD configuration

---
+++ APS_Control/project/inc/driver_tmr.h
@@ -0,0 +1,8 @@
+//
+// Created by pader on 14.08.2025.
+//
+
+#ifndef DRIVER_TMR_H
+#define DRIVER_TMR_H
+
+#endif //DRIVER_TMR_H
---
+++ APS_Control/project/src/driver_tmr.c
@@ -0,0 +1,5 @@
+//
+// Created by pader on 14.08.2025.
+//
+
+#include "driver_tmr.h"
